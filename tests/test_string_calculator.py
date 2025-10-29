e2411fdb5bf1cc3f830224b29b0f2b622dbf8acb
Salhi Abdo <salhiabdennour58@gmail.com>
2025-10-29 22:50:49 +0100

    Passing from "4" to "9" and return the same entered value

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -1,7 +1,17 @@
 import pytest
 from src.string_calculator import add_numbers_from_string
 
-@pytest.mark.parametrize('String,result_expected', [('',0),('0',0),('1',1),('2',2),('3',3)])
+@pytest.mark.parametrize('String,result_expected', [('',0),
+                                                    ('0',0),
+                                                    ('1',1),
+                                                    ('2',2),
+                                                    ('3',3),
+                                                    ('4',4),
+                                                    ('5',5),
+                                                    ('6',6),
+                                                    ('7',7),
+                                                    ('8',8),
+                                                    ('9',9),])
 def test_string_return_result_expected(String, result_expected):
     actual_result = add_numbers_from_string(String)
     assert actual_result == result_expected
