488bc86a2cb31136489fc17a47893d776e660f8f
Salhi Abdo <salhiabdennour58@gmail.com>
2025-10-29 22:22:06 +0100

    Passing "2"to the function return 2

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,4 +1,6 @@
 def add_numbers_from_string(String: str)->int:
     if String == "1":
         return 1
+    if String == "2":
+        return 2
     return 0
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -6,3 +6,6 @@ def test_passing_0_return_0():
     assert add_numbers_from_string("0") == 0
 def test_passing_1_return_1():
     assert add_numbers_from_string("1") == 1
+
+def test_passing_2_return_2():
+        assert add_numbers_from_string("2") == 2
