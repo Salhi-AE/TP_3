ecc42191855acc335961eda3e6a17d4775bbf7bf
Salhi Abdo <salhiabdennour58@gmail.com>
2025-10-29 21:54:01 +0100

    Passing 0 to the function return 0

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -2,3 +2,5 @@ from src.string_calculator import add_numbers_from_string
 
 def test_passing_empty_value_return_0():
     assert add_numbers_from_string("") == 0
+def test_passing_0_return_0():
+    assert add_numbers_from_string("0") == 0
